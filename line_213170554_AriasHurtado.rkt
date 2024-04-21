95c9ecde9a831979eca45c8bf49fc27dee7fbb1a
Jordan Arias <jordan.arias@usach.cl>
2024-04-20 23:48:27 -0400

    implementadas funciones para verificar consistencia de la construccion de una linea

--- line_213170554_AriasHurtado.rkt
+++ line_213170554_AriasHurtado.rkt
@@ -13,7 +13,7 @@
   (if (and (integer? id-line)
            (string? name-line)
            (string? rail-type)
-           
+           (compare-two-section )
            )
   (list id-line name-line rail-type section)
   null
@@ -22,6 +22,7 @@
 
 
 
+;compara que 2 secciones no sean iguales, e1 ab =! e2 ab
 (define (compare-two-section section1 section2)
   (not (and(member-station (get-point1-section section1) section2)
            (member-station (get-point2-section section1) section2)
@@ -29,14 +30,14 @@
        )
  )
 
-
+;compara el primer elemento con el resto
 (define (compare-section section sections)
   (and (compare-two-section section (car sections))
        (compare-section section (cdr sections))
       )
   )
   
-
+;compara todos los elementos con el llamado recursivo
 (define (repite-section? sections)
   (and (compare-section (car sections) (cdr sections))
        (repite-section? (cdr sections))
@@ -45,8 +46,120 @@
 
 
 
-;Creación de Línea 1 con todos los tramos
-(define l1 (line 1 "Línea 1" "UIC 60 ASCE" s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11 s12 s13 s14 s15))
-;Creación de Línea 2 sin incluir tramos
-(define l2 (line 2 "Línea 2" "100 R.E."))
+(define (get-station stations)
+  (car stations))
 
+(define (get-other-stations stations)
+  (car(cdr stations)))
+
+(define (get-routes stations)
+  (if (stations? stations)
+      ((get-other-stations stations))
+      (error)
+   )
+ )
+
+(define (get-station-candidate-route candidate-route)
+  (car candidate-route))
+
+(define (get-stations-candidate-route candidate-route)
+  (cdr candidate-route))
+
+(define (get-first-route routes)
+  (car routes))
+
+(define (get-second-route routes)
+  (cdr routes))
+
+(define (get-first-station stations)
+  (caar stations))
+
+(define (get-second-station stations)
+  (cadr stations))
+
+(define (get-stations-without-first stations)
+  (cdr stations))
+
+
+
+
+
+(define (remove-visited-stations stations visited-stations)
+  stations(filter  (lambda (x) (not (or (exist-in-line? (x) visited-stations)
+                                        (exist-in-line? (x) visited-stations)
+                                    )
+                    )
+           )stations)
+  )
+
+
+(define (other-route routes station)
+  (if (equal? (get-first-route routes) station)
+      (get-second-route routes)
+      (get-first-route routes)
+  )
+)
+
+
+(define (get-first-possible-candidates possible-candidates-routes)
+  (car possible-candidates-routes))
+
+(define (get-other-possible-candidates possible-candidates-routes)
+  (cdr possible-candidates-routes))
+
+(define (go-through-stations stations possible-candidates-routes station-start station-end visited-stations)
+  (if (null? possible-candidates-routes)
+      #f
+      (if (posible-to-arrive? stations (other-route (get-first-possible-candidates possible-candidates-routes) station-start) station-end visited-stations)
+          #t
+          (go-through-stations stations (get-other-possible-candidates possible-candidates-routes) station-end visited-stations)
+      )
+  )
+)
+
+
+(define (directly-connected? stations station-start station-end)
+  (if (null? stations)
+      #f
+      (if (or (and(equal?((get-first-station stations) station-start)(equal?(get-second-station stations)station-end))
+              (and(equal?((get-first-station stations) station-end)(equal?(get-second-station stations)station-start))
+                  )))
+           #t
+           (directly-connected? (get-stations-without-first stations) station-start station-end) 
+           )
+       ) 
+  )
+
+(define (stations? stations)
+  (list? stations))
+
+
+;verificar si la seccion esta en la linea
+(define (exist-in-line? station sections)
+  (if (equal? (member station sections) #f)
+      #f
+      #t)
+  )
+
+
+;funcion para ver si es posible llegar de una estacion a otra
+(define (posible-to-arrive? stations station-start station-end visited-stations)
+                              (if (and (exist-in-line? station-start (get-station stations))
+                                       (exist-in-line? station-end (get-station stations))
+                                       )
+                                  (if (equal? station-start station-end)
+                                      #t
+                                      (if (directly-connected? stations station-start station-end)
+                                          #t
+                                          (go-through-stations
+                                           stations
+                                           (remove-visited-stations (other-route (get-routes stations) station-start) visited-stations)
+                                           station-start
+                                           station-end
+                                           (cons station-start visited-stations)
+                                           )
+                                       )
+                               )
+                               (raise "no es posible llegar a la estacion")   
+         )
+ )
--- station_213170554_AriasHurtado.rkt
+++ station_213170554_AriasHurtado.rkt
@@ -48,34 +48,3 @@
            ) 
    )
  
-
-
-;Estaciones L1 simplificada metro santiago
-(define e0 (station 0 "San Pablo" "t" 90))
-(define e1 (station 1 "Neptuno" "r" 45))
-(define e2 (station 2 "Pajaritos" "c" 45))
-(define e3 (station 3 "Las Rejas" "r" 45))
-(define e4 (station 4 "Ecuador" "r" 60))
-(define e5 (station 5 "San Alberto Hurtado" "r" 40))
-(define e6 (station 6 "Universidad de Santiago de Chile" "c" 40))
-(define e7 (station 7 "Estación Central" "c" 45))
-(define e8 (station 8 "Unión Latinoamericana" "r" 30))
-(define e9 (station 9 "República" "r" 40))
-(define e10 (station 10 "Los Héroes" "c" 60))
-(define e11 (station 11 "La Moneda" "r" 40))
-(define e12 (station 12 "Universidad de Chile" "c" 90))
-(define e13 (station 13 "Santa Lucía" "r" 40))
-(define e14 (station 14 "Universidad Católica" "c" 60))
-(define e15 (station 15 "Baquedano" "r" 40))
-(define e16 (station 16 "Los Dominicos" "t" 90))
-(define e17 (station 17 "Cochera Neptuno" "m" 3600))
-
-;Estaciones L2 simplificada metro santiago, para una versión circular
-(define e18 (station 18 "El Llano" "r" 60))
-(define e19 (station 19 "Franklin" "r" 50))
-(define e20 (station 20 "Rondizzoni" "r" 55))
-(define e21 (station 21 "Parque O'Higgins" "r" 65))
-(define e22 (station 22 "Toesca" "r" 65))
-(define e23 (station 23 "Santa Ana" "c" 65))
-(define e24 (station 24 "Puente Cal y Canto" "r" 65))
-
